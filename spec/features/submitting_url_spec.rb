be7b7c7cf10163c2f745746e85dacbf832ce25b1
Michael Harrison <harrim91@hotmail.co.uk>
2016-10-02 00:04:31 +0100

    fixed tests

--- spec/features/submitting_url_spec.rb
+++ spec/features/submitting_url_spec.rb
@@ -3,18 +3,17 @@ feature 'Submitting URL', type: :feature do
 	before(:each) do
 		visit '/'
 		within 'form#form-url' do
-			fill_in :'field-url', with: 'http://www.google.com'
+			fill_in :'field-long-url', with: 'http://www.test.com'
 		end
 	end
 	
 	it 'saves the URL' do
-		expect{ click_button 'Shorten URL' }.to change(ShortURL, :count).by 1
+		expect{ click_button 'Shorten URL' }.to change(URL, :count).by 1
 	end
 
 	it 'returns a shortened URL' do
 		click_button 'Shorten URL'
 		expect(current_path).to eq '/'
-		id = ShortURL.last.id
-		expect(page).to have_content "#{(id + 1679615).to_s(36)}"
+		expect(page.find_field("field-short-url").value).to match "#{URL.last.id.to_s(36)}"
 	end
 end
